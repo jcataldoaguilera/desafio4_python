e71619a93d4051ca7723c8be005e7d1b48fd16c9
Juan Cataldo <jcataldoaguilera@gmail.com>
2024-03-21 23:45:47 -0300

    se crea archivo de actividad recordatorios.py

---
+++ Act3/recordatorios.py
@@ -0,0 +1,15 @@
+# Info
+# Autor: Juan Cataldo [jcataldoaguilera@gmail.com]
+# Fecha: 2024-03-22
+# RLAB-23-02-09-0044-2
+#
+
+# Data
+recordatorios = [['2021-01-01', "11:00", "Levantarse y ejercitar"],
+['2021-05-01', "15:00", "No trabajar"],
+['2021-07-15', "13:00", "No hacer nada es feriado"],
+['2021-09-18', "16:00", "Ramadas"],
+['2021-12-25', "00:00", "Navidad"]]
+
+# Output
+print(recordatorios)
